a9d1b8c4b4b9650978e031190f0811818fbe1860
Edwin Guilbert <edwin.guilbert@magnolia-cms.com>
2020-07-30 18:49:35 +0200

    image url fix

--- src/components/Image.vue
+++ src/components/Image.vue
@@ -1,10 +1,17 @@
 <template>
-  <img class="Image" v-bind:src="image['@link']" alt="Etiam Purus" />
+  <img class="Image" v-bind:src="IMAGE_BASE+image['@link']" alt="Etiam Purus" />
 </template>
 
 <script>
+const IMAGE_BASE = process.env.VUE_APP_DAM_BASE_URL;
+
 export default {
   name: "Image",
-  props: ["image"]
+  props: ["image"],
+  data() {
+    return {
+      imgURL: IMAGE_BASE
+    };
+  }
 };
 </script>
